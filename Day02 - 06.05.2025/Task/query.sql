3d082d3600d75ddbc99754782214c098c5d3d752
kavinkumar24 <kavinpalanisamy242003@gmail.com>
2025-05-06 18:09:51 +0530

    Task Updation with Practice Questions

--- Day02 - 06.05.2025/Task/query.sql
+++ Day02 - 06.05.2025/Task/query.sql
@@ -1,3 +1,10 @@
+-- Table Schema:	 
+-- Create Tables with Integrity Constrains:
+-- a)	EMP (empno - primary key, empname, salary, deptname - references entries in a deptname of department table with null constraint, bossno - references entries in an empno of emp table with null constraint)
+-- b)	DEPARTMENT (deptname - primary key, floor, phone, empno - references entries in an empno of emp table not null)
+-- c)	SALES (salesno - primary key, saleqty, itemname -references entries in a itemname of item table with not null constraint, deptname - references entries in a deptname of department table with not null constraint)
+-- d)	ITEM (itemname - primary key, itemtype, itemcolor)
+
 
 CREATE TABLE EMP(
  empno INT PRIMARY KEY,
