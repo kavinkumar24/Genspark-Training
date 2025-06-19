0c3ab8a3b959a479c03cff520d54a62743974adc
Presidio <kavinpalanisamy242003@gmail.com>
2025-06-19 18:32:30 +0530

    Day 34 task update

--- Day34 - 19.06.2025/Task/UserManagement/src/app/app.routes.ts
+++ Day34 - 19.06.2025/Task/UserManagement/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { AddUser } from './add-user/add-user';
 import { UserDashboard } from './user-dashboard/user-dashboard';
 
 export const routes: Routes = [
-
+    {path:'', redirectTo:'/dashboard', pathMatch:'full'},
     {path:'dashboard', component: UserDashboard}
 ];
